06d02d431f9fe9474d125ceee4c9638c9520a49a
Adam Dobrzycki <adamd@picnet.com.au>
2016-11-16 14:47:09 +1100

    Added simple writeline to Program.cs

--- XGBoost/Program.cs
+++ XGBoost/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace XGBoost
 {
@@ -10,6 +6,8 @@ namespace XGBoost
   {
     static void Main(string[] args)
     {
+      Console.WriteLine("Program.Main()");
+      Console.ReadKey();
     }
   }
 }
