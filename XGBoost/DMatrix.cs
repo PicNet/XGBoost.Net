82d5f104eb28feca6c7ed52718ef98205340fe64
Adam Dobrzycki <adamd@picnet.com.au>
2016-11-16 15:14:42 +1100

    Added DMatrix.cs

---
+++ XGBoost/DMatrix.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XGBoost
+{
+  class DMatrix
+  {
+    public string[] FeatureNames()
+    {
+      return null;
+    }
+
+    public string[] FeatureTypes()
+    {
+      return null;
+    }
+
+    public float GetBaseMargin()
+    {
+      return 0;
+    }
+
+    public float[][] GetFloatInfo()
+    {
+      return null;
+    }
+
+    public string[] GetLabel()
+    {
+      return null;
+    }
+
+    public float[][] GetUIntInfo()
+    {
+      return null;
+    }
+
+    public float[] GetWeight()
+    {
+      return null;
+    }
+
+    public int NumCol()
+    {
+      return 0;
+    }
+
+    public int NumRow()
+    {
+      return 0;
+    }
+
+    public void SaveBinary()
+    {
+
+    }
+
+    public void SetBaseMargin()
+    {
+
+    }
+
+    public void SetFloatInfo()
+    {
+
+    }
+
+    public void SetGroup()
+    {
+
+    }
+
+    public void SetLabel()
+    {
+
+    }
+
+    public void SetUIntInfo()
+    {
+
+    }
+
+    public void SetWeight()
+    {
+
+    }
+
+    public DMatrix Slice()
+    {
+      return null;
+    }
+  }
+}
