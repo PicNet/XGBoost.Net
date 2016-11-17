78342176e603ca3ba7da73333943f09eb7ace6fc
Adam Dobrzycki <adamd@picnet.com.au>
2016-11-18 10:35:11 +1100

    added demo project (which isn't working because of dllimport)

---
+++ Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using XGBoost;
+
+namespace Demo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Program.Main() (Demo)");
+      DMatrix d = new DMatrix("C:/Users/adamd/Documents/Visual Studio 2015/Projects/guidoXgboost/guidoXgboost/libs/test.txt");
+      int cols = d.NumCol();
+      Console.WriteLine("cols = " + cols.ToString());
+      Console.ReadKey();
+    }
+  }
+}
--- XGBoostTests/DMatrixTests.cs
+++ XGBoostTests/DMatrixTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace XGBoostTests
 {
   [TestClass]
-  public class UnitTest1
+  public class DMatrixTests
   {
     [TestMethod]
     public void TestMethod1()
