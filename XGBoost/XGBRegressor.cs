aedb4098e8b955d3e567a27b427fce32f1891786
Adam Dobrzycki <adamd@picnet.com.au>
2016-11-22 12:13:00 +1100

    added XGBRegressor

---
+++ XGBoost/XGBRegressor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XGBoost
+{
+  class XGBRegressor
+  {
+    public XGBRegressor()
+    { 
+    }
+
+    public void fit(float[][] data, float[] labels)
+    {
+      
+    }
+  }
+}
