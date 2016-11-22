3a251cbaf375e6b81882d095dd57c5f14e59653d
Adam Dobrzycki <adamd@picnet.com.au>
2016-11-23 09:39:09 +1100

    refactored Main program to be more readable for demo purposes

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -7,17 +7,44 @@ namespace Demo
   {
     static void Main(string[] args)
     {
+      float[][] dataTrain = GetDataTrain();
+      float[] labels = GetLabels();
+      float[][] dataTest = GetDataTest();
+
+      XGBRegressor xgbr = new XGBRegressor();
+      xgbr.Fit(dataTrain, labels);
+      float[] preds = xgbr.Predict(dataTest);
+      PrintPreds(preds);
+    }
+
+    static float[][] GetDataTrain()
+    {
       float[][] dataTrain = new float[2][];
       dataTrain[0] = new float[1];
       dataTrain[1] = new float[1];
       dataTrain[0][0] = 0;
       dataTrain[1][0] = 1;
-      float[] labels = { 0, 1 };
+      return dataTrain;
+    }
 
-      XGBRegressor xgbr = new XGBRegressor();
-      xgbr.Fit(dataTrain, labels);
-      float[][] dataTest = dataTrain;
-      float[] preds = xgbr.Predict(dataTest);
+    static float[] GetLabels()
+    {
+      float[] labels = { 0, 1 };
+      return labels;
+    }
+
+    static float[][] GetDataTest()
+    {
+      float[][] dataTrain = new float[2][];
+      dataTrain[0] = new float[1];
+      dataTrain[1] = new float[1];
+      dataTrain[0][0] = 0;
+      dataTrain[1][0] = 1;
+      return dataTrain;
+    }
+
+    static void PrintPreds(float[] preds)
+    {
       for (int i = 0; i < preds.Length; i++)
       {
         Console.Write(preds[i].ToString() + " ");
