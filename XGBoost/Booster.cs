6c6e4edcb454824d50013f6611a6e6e1d11ecb03
Adam Dobrzycki <adamd@picnet.com.au>
2016-11-22 11:38:25 +1100

    Added empty Booster class

---
+++ XGBoost/Booster.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Win32.SafeHandles;
+
+namespace XGBoost
+{
+  public class Booster : IDisposable
+  {
+    private BoosterHandle _handle;
+
+    public Booster(float[][] data)
+    {
+      
+    }
+
+    public void Dispose()
+    {
+      if (_handle != null && !_handle.IsInvalid)
+      {
+        _handle.Dispose();
+      }
+    }
+  }
+
+  internal class BoosterHandle : SafeHandleZeroOrMinusOneIsInvalid
+  {
+    private BoosterHandle()
+        : base(true)
+    {
+    }
+
+    override protected bool ReleaseHandle()
+    {
+      return true;
+    }
+  }
+}
