a6ed756d65ff3a67c0c29a0aef44e3d896a1d83e
Adam Dobrzycki <adamd@picnet.com.au>
2016-11-25 13:04:57 +1100

    added empty XGBRegressorTest

---
+++ XGBoostTests/XGBRegressorTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XGBoost;
+
+namespace XGBoostTests
+{
+    [TestClass]
+    public class XGBRegressorTests
+    {
+        [TestMethod]
+        public void Predict()
+        {
+            float[][] dataTrain = GetDataTrain();
+            float[] labelsTrain = GetLabelsTrain();
+            float[][] dataTest = GetDataTest();
+
+            XGBRegressor xgbr = new XGBRegressor();
+            xgbr.Fit(dataTrain, labelsTrain);
+            float[] preds = xgbr.Predict(dataTest);
+            Assert.IsTrue(PredsCorrect(preds));
+        }
+
+        private float[][] GetDataTrain()
+        {
+            return null;
+        }
+
+        private float[] GetLabelsTrain()
+        {
+            return null;
+        }
+
+        private float[][] GetDataTest()
+        {
+            return null;
+        }
+
+        private bool PredsCorrect(float[] preds)
+        {
+            return true;
+        }
+    }
+}
