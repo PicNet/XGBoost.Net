408e7bbea02a93045f443ab43f851273d68bd763
Adam Dobrzycki <adamd@picnet.com.au>
2016-11-28 10:07:27 +1100

    added comments to XGBClassifier

--- XGBoost/XGBClassifier.cs
+++ XGBoost/XGBClassifier.cs
@@ -9,13 +9,71 @@ namespace XGBoost
     private readonly IDictionary<string, object> parameters = new Dictionary<string, object>();
     private Booster booster;
 
+    /// <summary>
+    ///   Implementation of the Scikit-Learn API for XGBoost
+    /// </summary>
+    /// <param name="maxDepth">
+    ///   Maximum tree depth for base learners
+    /// </param>
+    /// <param name="learningRate">
+    ///   Boosting learning rate (xgb's "eta")
+    /// </param>
+    /// <param name="nEstimators">
+    ///   Number of boosted trees to fit
+    /// </param>
+    /// <param name="silent">
+    ///   Whether to print messages while running boosting
+    /// </param>
+    /// <param name="objective">
+    ///   Specify the learning task and the corresponding learning objective or
+    ///   a custom objective function to be used(see note below)
+    /// </param>
+    /// <param name="nThread">
+    ///   Number of parallel threads used to run xgboost
+    /// </param>
+    /// <param name="gamma">
+    ///   Minimum loss reduction required to make a further partition on a leaf node of the tree
+    /// </param>
+    /// <param name="minChildWeight">
+    ///   Minimum sum of instance weight(hessian) needed in a child
+    /// </param>
+    /// <param name="maxDeltaStep">
+    ///   Maximum delta step we allow each tree's weight estimation to be
+    /// </param>
+    /// <param name="subsample">
+    ///   Subsample ratio of the training instance
+    /// </param>
+    /// <param name="colSampleByTree">
+    ///   Subsample ratio of columns when constructing each tree
+    /// </param>
+    /// <param name="colSampleByLevel">
+    ///   Subsample ratio of columns for each split, in each level
+    /// </param>
+    /// <param name="regAlpha">
+    ///   L1 regularization term on weights
+    /// </param>
+    /// <param name="regLambda">
+    ///   L2 regularization term on weights
+    /// </param>
+    /// <param name="scalePosWeight">
+    ///   Balancing of positive and negative weights
+    /// </param>
+    /// <param name="baseScore">
+    ///   The initial prediction score of all instances, global bias
+    /// </param>
+    /// <param name="seed">
+    ///   Random number seed
+    /// </param>
+    /// <param name="missing">
+    ///   Value in the data which needs to be present as a missing value
+    /// </param>
     public XGBClassifier(int maxDepth = 3, float learningRate = 0.1F, int nEstimators = 100,
-                        bool silent = true, string objective = "binary:logistic",
-                        int nThread = -1, float gamma = 0, int minChildWeight = 1,
-                        int maxDeltaStep = 0, float subsample = 1, float colSampleByTree = 1,
-                        float colSampleByLevel = 1, float regAlpha = 0, float regLambda = 1,
-                        float scalePosWeight = 1, float baseScore = 0.5F, int seed = 0,
-                        float missing = float.NaN)
+                    bool silent = true, string objective = "binary:logistic",
+                    int nThread = -1, float gamma = 0, int minChildWeight = 1,
+                    int maxDeltaStep = 0, float subsample = 1, float colSampleByTree = 1,
+                    float colSampleByLevel = 1, float regAlpha = 0, float regLambda = 1,
+                    float scalePosWeight = 1, float baseScore = 0.5F, int seed = 0,
+                    float missing = float.NaN)
     {
       parameters["max_depth"] = maxDepth;
       parameters["learning_rate"] = learningRate;
@@ -40,25 +98,30 @@ namespace XGBoost
       parameters["_Booster"] = null;
     }
 
-    /*
-     * TODO: Most of these paramaters probably don't work now since their behaviour needs to
-     *     public void Fit(float[][] data, float[] labels, float[][] evalSet = null,
-                    string evalMetric = null, int? earlyStoppingRounds = null,
-                    bool verbose = true)
-     * be implemented. Only the essential parameters are actually implemented
-     */
+    /// <summary>
+    ///   Fit the gradient boosting model
+    /// </summary>
+    /// <param name="data">
+    ///   Feature matrix
+    /// </param>
+    /// <param name="labels">
+    ///   Labels
+    /// </param>
     public void Fit(float[][] data, float[] labels)
     {
       var train = new DMatrix(data, labels);
       booster = Train(parameters, train, ((int)parameters["n_estimators"]));
     }
 
-    /*
-     * TODO: Most of these paramaters probably don't work now since their behaviour needs to
-     *     public float[] Predict(float[][] data, bool outputMargin = false,
-                           int nTreeLimit = 0)
-     * be implemented. Only the essential parameters are actually implemented
-     */
+    /// <summary>
+    ///   Predict using the gradient boosted model
+    /// </summary>
+    /// <param name="data">
+    ///   Feature matrix to do predicitons on
+    /// </param>
+    /// <returns>
+    ///   Predictions
+    /// </returns>
     public float[] Predict(float[][] data)
     {
       var test = new DMatrix(data);
@@ -66,24 +129,24 @@ namespace XGBoost
       return preds.Select(v => v > 0.5f ? 1f : 0f).ToArray();
     }
 
-    /*
-     * float[][] data, bool outputMargin = false, int nTreeLimit = 0
-     */
+    /// <summary>
+    ///   Predict using the gradient boosted model
+    /// </summary>
+    /// <param name="data">
+    ///   Feature matrix to do predicitons on
+    /// </param>
+    /// <returns>
+    ///   The probabilities for each classification being the actual
+    ///   classification for each row
+    /// </returns>
     public float[][] PredictProba(float[][] data)
     {
       var dTest = new DMatrix(data);
       var preds = booster.Predict(dTest);
-            //return preds.Select(v => v > 0.5f ? new [] {v, 1 - v} : new[] { 1 - v, v }).ToArray();
       return preds.Select(v => new[] { 1 - v, v } ).ToArray();
     }
     
-    private Booster Train(IDictionary<string, object> args, DMatrix dTrain,
-                         int numBoostRound = 10, Tuple<DMatrix, string>[] evals = null,
-                         Func<string, float> obj = null, Func<float, float> fEval = null,
-                         bool maximize = false, int? earlyStoppingRounds = null,
-                         Object evalsResult = null,
-                         bool verboseEval = true, Object learningRates = null,
-                         string xgbModel = null, Object[] callbacks = null)
+    private Booster Train(IDictionary<string, object> args, DMatrix dTrain, int numBoostRound = 10)
     {
       var bst = new Booster(args, dTrain);
       for (int i = 0; i < numBoostRound; i++) { bst.Update(dTrain, i); }
