eb90f5afc2c50d784daed1fe334a4e93b5cdc1b8
Adam Dobrzycki <adamd@picnet.com.au>
2016-11-30 11:51:37 +1100

    changed how libxgboost dll imported

--- XGBoost/XGBOOST_NATIVE_METHODS.cs
+++ XGBoost/XGBOOST_NATIVE_METHODS.cs
@@ -5,36 +5,36 @@ namespace XGBoost
 {
   public class XGBOOST_NATIVE_METHODS
   {
-    [DllImport("lib/libxgboost.dll")]
+    [DllImport("libxgboost.dll")]
     public static extern int XGDMatrixCreateFromMat(float[] data, ulong nrow, ulong ncol, 
                                                     float missing, out IntPtr handle);
 
-    [DllImport("lib/libxgboost.dll")]
+    [DllImport("libxgboost.dll")]
     public static extern int XGDMatrixFree(IntPtr handle);
 
-    [DllImport("lib/libxgboost.dll")]
+    [DllImport("libxgboost.dll")]
     public static extern int XGDMatrixGetFloatInfo(IntPtr handle, string field, 
                                                    out ulong len, out IntPtr result);
 
-    [DllImport("lib/libxgboost.dll")]
+    [DllImport("libxgboost.dll")]
     public static extern int XGDMatrixSetFloatInfo(IntPtr handle, string field,
                                                    float[] array, ulong len);
 
-    [DllImport("lib/libxgboost.dll")]
+    [DllImport("libxgboost.dll")]
     public static extern int XGBoosterCreate(IntPtr[] dmats, 
                                              ulong len, out IntPtr handle);
 
-    [DllImport("lib/libxgboost.dll")]
+    [DllImport("libxgboost.dll")]
     public static extern int XGBoosterFree(IntPtr handle);
 
-    [DllImport("lib/libxgboost.dll")]
+    [DllImport("libxgboost.dll")]
     public static extern int XGBoosterSetParam(IntPtr handle, string name, string val);
 
-    [DllImport("lib/libxgboost.dll")]
+    [DllImport("libxgboost.dll")]
     public static extern int XGBoosterUpdateOneIter(IntPtr bHandle, int iter, 
                                                     IntPtr dHandle);
 
-    [DllImport("lib/libxgboost.dll")]
+    [DllImport("libxgboost.dll")]
     public static extern int XGBoosterPredict(IntPtr bHandle, IntPtr dHandle, 
                                               int optionMask, int ntreeLimit, 
                                               out ulong predsLen, out IntPtr predsPtr);
